13297a5bf3c1b9efb3f01c660d53712fc01cdf77
huzaif137 <huzaifabukenya227@gmail.com>
2022-04-17 10:49:26 +0530

    IshakCOmmunityFoundation frontend-day

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.app')
+@section('content')
+
+<section id="inner-banner">
+<div class="overlay">
+<div class="container">
+<div class="row"> 
+<div class="col-sm-6"><h1>ABOUT US</h1></div>
+<div class="col-sm-6">
+  <h6 class="breadcrumb"><a href="/">Home</a> / About us</h6></div>
+</div>
+</div>
+</div>
+</section>
+
+<section id="about-sec">
+<div class="container">
+<div class="row text-center">
+<h1>ABOUT CHARITY HOPE</h1>
+<hr>
+
+<div class="act-box clearfix">
+  <div class="col-md-6">
+  <div class="image"><img src="/assets/images/logo_png.png" /></div>
+  </div>
+  <div class="col-md-6">
+  <div class="act-pad">
+  <h4>Sponsor a child today</h4>
+  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+     Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form,
+     accompanied by English versions from the 1914 translation by H. Rackham.Lorem ipsum dolor sit amet, consectet ur adipisicing. Odit qui minima praese ntium obcaecati minima praesent  Odit qui minima praese ntium obcaecati minima Odit qui minima praese ntium obcaecati minima</p>
+  <div class="price">Raised: $1890 <span class="goal">Goal: $2500</span></div>
+  <a href="donate" class="btn1">donate now</a>
+  <div class="clearfix"></div>
+  </div>
+  </div>
+  </div>
+  <br> <br>
+<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.</p>
+<div class="text-center">
+</div>
+</div>
+</div>
+</section>
+
+<section id="help">
+<div class="container">
+<div class="row text-center">
+<h1>WAYS TO HELP</h1>
+<hr>
+<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+<div class="text-left"> 
+<div class="col-md-6 clearfix top-off">
+<div class="icon_circle"><i class="fa fa-heartbeat">&nbsp;</i></div>
+<div class="help-text">
+<h4>Donate</h4>
+<div class="line line-50"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</div>
+<div class="col-md-6 clearfix top-off">
+<div class="icon_circle"><i class="fa fa-user-plus">&nbsp;</i></div>
+<div class="help-text">
+<h4>Participate</h4>
+<div class="line line-50"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</div>
+<div class="col-md-6 clearfix top-off">
+<div class="icon_circle"><i class="fa fa-usd">&nbsp;</i></div>
+<div class="help-text">
+<h4>Fundraise</h4>
+<div class="line line-50"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</div>
+<div class="col-md-6 clearfix top-off">
+<div class="icon_circle"><i class="fa fa-credit-card">&nbsp;</i></div>
+<div class="help-text">
+<h4>Contribute</h4>
+<div class="line line-50"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section id="team">
+<div class="container">
+<div class="row text-center">
+<h1>MEET OUR TEAM</h1>
+<hr>
+<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+<div class="member col-xs-6 col-md-3">
+<div class="inner">
+<div class="avatar-inner"><img src="/assets/images/team1.jpg"></div>
+<div class="info"><div class="name"><a href="#"> Michel Black </a></div><div class="regency">Volunteer</div></div>
+</div>
+</div>
+
+<div class="member col-xs-6 col-md-3">
+<div class="inner">
+<div class="avatar-inner"><img src="/assets/images/team2.jpg"></div>
+<div class="info"><div class="name"><a href="#"> Juliana Moor </a></div><div class="regency">Volunteer</div></div>
+</div>
+</div>
+
+<div class="member col-xs-6 col-md-3">
+<div class="inner">
+<div class="avatar-inner"><img src="/assets/images/team3.jpg"></div>
+<div class="info"><div class="name"><a href="#"> Andre Russel</a></div><div class="regency">Volunteer</div></div>
+</div>
+</div>
+
+<div class="member col-xs-6 col-md-3">
+<div class="inner">
+<div class="avatar-inner"><img src="/assets/images/team4.jpg"></div>
+<div class="info"><div class="name"><a href="#"> Amily James</a></div><div class="regency">Volunteer</div></div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::view('/', 'index');
+Route::view('temp', 'temp');
+Route::view('donate', 'donate');
+Route::view('sponsor', 'sponsor');
+Route::view('aboutus', 'aboutus');
+Route::view('gallery', 'gallery');
+Route::view('contact', 'contact');
+Route::view('projects', 'projects');
+Route::view('activities', 'activities');
+Route::view('sponsor-a-child', 'sponsor-a-child');
+
